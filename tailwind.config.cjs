dc506bb0f14139214640d3fb3839f007877948e5
alexandra trivino <alexandratrivino2@gmail.com>
2022-10-08 18:49:43 -0500

    Final config of tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.jsx"], /// el ** indica todos los archivos de ese directorio y luego *.jsx todos los del sig directorio que sean jsx
   theme: {
     extend: {},
   },
