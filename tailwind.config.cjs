1307c9d2defd256098c991ac9c65f78bbf5ce84a
alexandra trivino <alexandratrivino2@gmail.com>
2022-10-10 20:51:57 -0500

    modifying tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.jsx"],
+  purge: ["./index.html", "./src/**/*.jsx"],
   theme: {
     extend: {},
   },
